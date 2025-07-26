a69898bcd5b801393f126effbe9d3fa394eda2a8
Halit Derya <halitderyaa@gmail.com>
2025-07-26 11:05:49 +0100

    terraform second

--- variables.tf
+++ variables.tf
@@ -1,11 +1,12 @@
 variable "s3_bucket_name" {
   description = "The name of the S3 bucket for image upload"
   type        = string
+  default     = "halit-imageupload-app-2025"  
 }
 
-variable "aws_region"{
-    description="AWS Region"
-    type=       string
+variable "aws_region" {
+    description = "AWS Region"
+    type        = string
     default     = "eu-west-2"
 }
 
