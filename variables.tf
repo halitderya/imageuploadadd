e36622c030291f1f40ca21d7bb822556b7317426
Halit Derya <halitderyaa@gmail.com>
2025-07-26 10:37:07 +0100

    terraform second

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "s3_bucket_name" {
   type        = string
 }
 
+variable "aws_region"{
+    description="AWS Region"
+    type="string"
+}
+
 variable "environment" {
   description = "Deployment environment name"
   type        = string
