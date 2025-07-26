6dc58506de2f9730d9f41537b1dabb28fdbc642e
Halit Derya <halitderyaa@gmail.com>
2025-07-26 10:35:28 +0100

    terraform second

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket for image upload"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment name"
+  type        = string
+  default     = "dev"
+}
